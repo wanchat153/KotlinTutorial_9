b1c88ada6239d841f94a73669efeaf032709411e
Wanchat <wanchatgame@gmail.com>
2019-10-17 20:42:07 +0700

    1. สร้าง Log ช่ือ TAG
    2.
    สร้าง override fun doInBackground
    
    3.
    ใส่ url flickr
    สร้าง fun onDownloadComplete
    
    4.
    Callbacks
    
    5.
    สร้าง class Photo
    
    6.
    สร้าง class GetFlickrJsonData
    
    7.
    แก้ไข code GetFlickrJsonData
    
    8.
    เพิ่ม fun Uri
    
    9.
    เพิ่ม reyclerview ใน content_main
    
    10.
    เพิ่มรูป และสร้าง layout browse
    
    11.
    ตั้งค่า layout ความสูงและความยาว
    
    12.
    สร้าง PhotoDetailsActivity, SearchActivity และเข้าไปแก้ content_photo_datails ตั้งเป็น ScrollView
    
    13.
    จัดหน้า content_photo_datails
    
    14.
    สร้าง class FlickRecyclerViewHolder, FlickRecyclerViewAdaptor
    
    15.
    เพิ่ม ปลั๊กอืน gradle
    cardview-v7:28.0.0
    appcompat-v7:28.0.0
    picasso:2.5.2
    
    16.
    Adapter ไปยัง Recylerview
    แก้โค้ดบ่างส่วน แก้id imageview เป็น thumbnail ทีืลืมเปลี่ยนตั้งแต่แรกใน browse และ utl แก้เป็น url
    
    17.
    สร้าง class RecyclerItemClickListener
    
    18.
    ประกาศใช้ gestureDetector
    
    19.
    ทำให้ display full photo
    
    20.
    เพิ่ม Serializable และแก้ import ที่ไม่ได้ใช้ออก
    
    21.
    ประกาศ val photo ใน PhotoDetailsActivity

--- app/src/main/java/com/example/kotlintutorial_9/PhotoDetailsActivity.kt
+++ app/src/main/java/com/example/kotlintutorial_9/PhotoDetailsActivity.kt
@@ -1,6 +1,8 @@
 package com.example.kotlintutorial_9
 
 import android.os.Bundle
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.content_photo_details.*
 
 class PhotoDetailsActivity : BaseActivity() {
 
@@ -8,6 +10,16 @@ class PhotoDetailsActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_details)
         activateToolbar(true)
+
+        val photo = intent.getSerializableExtra(PHOTO_TRANSFER)as Photo
+        photo_title.text = photo.title
+        photo_tags.text = photo.tags
+        photo_author.text = photo.author
+
+        Picasso.with(this).load(photo.link)
+            .error(R.drawable.placeholder)
+            .placeholder(R.drawable.placeholder)
+            .into(photo_image)
     }
 
 }
