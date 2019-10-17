53bd1890b419400eec5dfe14843004a3391fcc77
Wanchat <wanchatgame@gmail.com>
2019-10-18 00:05:58 +0700

    1. สร้าง Log ช่ือ TAG
    2.
    สร้าง override fun doInBackground
    
    3.
    ใส่ url flickr
    สร้าง fun onDownloadComplete
    
    4.
    Callbacks
    
    5.
    สร้าง class Photo
    
    6.
    สร้าง class GetFlickrJsonData
    
    7.
    แก้ไข code GetFlickrJsonData
    
    8.
    เพิ่ม fun Uri
    
    9.
    เพิ่ม reyclerview ใน content_main
    
    10.
    เพิ่มรูป และสร้าง layout browse
    
    11.
    ตั้งค่า layout ความสูงและความยาว
    
    12.
    สร้าง PhotoDetailsActivity, SearchActivity และเข้าไปแก้ content_photo_datails ตั้งเป็น ScrollView
    
    13.
    จัดหน้า content_photo_datails
    
    14.
    สร้าง class FlickRecyclerViewHolder, FlickRecyclerViewAdaptor
    
    15.
    เพิ่ม ปลั๊กอืน gradle
    cardview-v7:28.0.0
    appcompat-v7:28.0.0
    picasso:2.5.2
    
    16.
    Adapter ไปยัง Recylerview
    แก้โค้ดบ่างส่วน แก้id imageview เป็น thumbnail ทีืลืมเปลี่ยนตั้งแต่แรกใน browse และ utl แก้เป็น url
    
    17.
    สร้าง class RecyclerItemClickListener
    
    18.
    ประกาศใช้ gestureDetector
    
    19.
    ทำให้ display full photo
    
    20.
    เพิ่ม Serializable และแก้ import ที่ไม่ได้ใช้ออก
    
    21.
    ประกาศ val photo ใน PhotoDetailsActivity
    
    22.
    แก้ไขหน้า Photo เพิ่ม Parcelable
    
    23.
    เพิ่มสัเปลี่ยนสีใน styles และลบ colors
    
    24.
    แก้สีไม่แสดง v23
    
    25.
    เปลี่ยนสี AppTheme.NoActionBar
    
    26.
    สร้าง material_design_colours ไว้เก็บค่าสี
    และ เพิ่ม V21
    
    27.
    แก้ edittext, ปรับimage
    
    28.
    แก้โค้ด String

--- app/src/main/java/com/example/kotlintutorial_9/PhotoDetailsActivity.kt
+++ app/src/main/java/com/example/kotlintutorial_9/PhotoDetailsActivity.kt
@@ -11,12 +11,17 @@ class PhotoDetailsActivity : BaseActivity() {
         setContentView(R.layout.activity_photo_details)
         activateToolbar(true)
 
-        //val photo = intent.getSerializableExtra(PHOTO_TRANSFER)as Photo
+        //val photo = intent.getSerializableExtra(PHOTO_TRANSFER) as Photo
         val photo = intent.extras?.getParcelable<Photo>(PHOTO_TRANSFER) as Photo
 
-        photo_title.text = photo.title
-        photo_tags.text = photo.tags
-        photo_author.text = photo.author
+//        photo_title.text = "Title: " + photo.title
+//        photo_tags.text = "Tags: " + photo.tags
+        photo_title.text = resources.getString(R.string.photo_title_text, photo.title)
+        photo_tags.text = resources.getString(R.string.photo_tags_text, photo.tags)
+
+        //photo_author.text = photo.author
+
+        photo_author.text = resources.getString(R.string.photo_author_text, "my", "rad", "car")
 
         Picasso.with(this).load(photo.link)
             .error(R.drawable.placeholder)
